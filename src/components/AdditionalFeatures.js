d1e821d21b63e9a55a8b2564e6dd0c9577b4cf65
mikecrbenton <mikecrbenton@hotmail.com>
2020-08-25 21:06:41 -0500

    mvp finished

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from 'react-redux';
 
 const AdditionalFeatures = props => {
+
+   console.log("ADDITIONAL FEATURES PROPS: ", props);
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+          {props.additionalFeatures.map( feature => (
+            <AdditionalFeature 
+               key={feature.id} 
+               feature={feature}
+            />
           ))}
         </ol>
       ) : (
@@ -18,4 +25,12 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = (state) => {
+   return {
+      additionalFeatures : state.additionalFeatures
+   }
+}
+// DON'T NEED DISPATCH IN THIS COMPONENT 
+// IMPORTED DIRECTLY INTO CHILD OF THIS COMPONENT
+
+export default connect( mapStateToProps, {} )(AdditionalFeatures);
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+
+// NOT USING THIS FILE
+
+// import { combineReducers } from 'redux';
+
+// // IMPORT THE NEEDED REDUCERS FROM THIS DIRECTORY
+// import { carReducer } from './carReducer.js';
+
+// export const rootReducer = combineReducers({
+//    carReducer
+// });
